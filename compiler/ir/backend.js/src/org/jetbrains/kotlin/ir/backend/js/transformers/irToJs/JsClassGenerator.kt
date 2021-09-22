e2bac296f3b34eac13c251f2a095bd814c1c3f27
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2021-09-22 11:05:39 +0300

    Revert "~ Error for nested classes in JsClassGenerator"
    This reverts commit 649d67232b63440fa6d7a4ef87b9c7f3b77c9da0.

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/JsClassGenerator.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/JsClassGenerator.kt
@@ -84,7 +84,7 @@ class JsClassGenerator(private val irClass: IrClass, val context: JsGenerationCo
                     }
                 }
                 is IrClass -> {
-                    error("Nested classes must be lowered at this point: ${declaration.fqNameWhenAvailable}")
+//                    classBlock.statements += JsClassGenerator(declaration, context).generate()
                 }
                 is IrField -> {
                 }
