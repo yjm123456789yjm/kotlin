921c6a6309dcd2cf274efdc73fcd66d14671a176
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2021-10-04 14:43:54 +0300

    Commonize JS suspend function lowering

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/coroutines/JsSuspendFunctionsLowering.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/coroutines/JsSuspendFunctionsLowering.kt
@@ -10,6 +10,7 @@ import org.jetbrains.kotlin.backend.common.ir.isSuspend
 import org.jetbrains.kotlin.backend.common.lower.FinallyBlocksLowering
 import org.jetbrains.kotlin.ir.backend.js.JsStatementOrigins
 import org.jetbrains.kotlin.backend.common.lower.ReturnableBlockTransformer
+import org.jetbrains.kotlin.ir.backend.js.JsCommonBackendContext
 import org.jetbrains.kotlin.ir.backend.js.JsIrBackendContext
 import org.jetbrains.kotlin.ir.backend.js.ir.JsIrBuilder
 import org.jetbrains.kotlin.ir.builders.*
@@ -29,16 +30,25 @@ import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.utils.DFS
 import org.jetbrains.kotlin.utils.addToStdlib.assertedCast
 
-class JsSuspendFunctionsLowering(ctx: JsIrBackendContext) : AbstractSuspendFunctionsLowering<JsIrBackendContext>(ctx) {
 
-    private val coroutineImplExceptionPropertyGetter = ctx.coroutineImplExceptionPropertyGetter
-    private val coroutineImplExceptionPropertySetter = ctx.coroutineImplExceptionPropertySetter
-    private val coroutineImplExceptionStatePropertyGetter = ctx.coroutineImplExceptionStatePropertyGetter
-    private val coroutineImplExceptionStatePropertySetter = ctx.coroutineImplExceptionStatePropertySetter
-    private val coroutineImplLabelPropertySetter = ctx.coroutineImplLabelPropertySetter
-    private val coroutineImplLabelPropertyGetter = ctx.coroutineImplLabelPropertyGetter
-    private val coroutineImplResultSymbolGetter = ctx.coroutineImplResultSymbolGetter
-    private val coroutineImplResultSymbolSetter = ctx.coroutineImplResultSymbolSetter
+fun JsSuspendFunctionsLowering(ctx: JsIrBackendContext) =
+    JsCommonSuspendFunctionsLowering(ctx, ctx.dynamicType)
+
+class JsCommonSuspendFunctionsLowering(
+    ctx: JsCommonBackendContext,
+    val throwableType: IrType,
+) : AbstractSuspendFunctionsLowering<JsCommonBackendContext>(ctx) {
+
+    val coroutineSymbols = ctx.coroutineSymbols
+
+    private val coroutineImplExceptionPropertyGetter = coroutineSymbols.coroutineImplExceptionPropertyGetter
+    private val coroutineImplExceptionPropertySetter = coroutineSymbols.coroutineImplExceptionPropertySetter
+    private val coroutineImplExceptionStatePropertyGetter = coroutineSymbols.coroutineImplExceptionStatePropertyGetter
+    private val coroutineImplExceptionStatePropertySetter = coroutineSymbols.coroutineImplExceptionStatePropertySetter
+    private val coroutineImplLabelPropertySetter = coroutineSymbols.coroutineImplLabelPropertySetter
+    private val coroutineImplLabelPropertyGetter = coroutineSymbols.coroutineImplLabelPropertyGetter
+    private val coroutineImplResultSymbolGetter = coroutineSymbols.coroutineImplResultSymbolGetter
+    private val coroutineImplResultSymbolSetter = coroutineSymbols.coroutineImplResultSymbolSetter
 
     private var coroutineId = 0
 
@@ -53,7 +63,7 @@ class JsSuspendFunctionsLowering(ctx: JsIrBackendContext) : AbstractSuspendFunct
         argumentToPropertiesMap: Map<IrValueParameter, IrField>
     ) {
         val returnableBlockTransformer = ReturnableBlockTransformer(context)
-        val finallyBlockTransformer = FinallyBlocksLowering(context, context.dynamicType)
+        val finallyBlockTransformer = FinallyBlocksLowering(context, throwableType)
         val simplifiedFunction =
             transformingFunction.transform(finallyBlockTransformer, null).transform(returnableBlockTransformer, null) as IrFunction
 
@@ -238,10 +248,9 @@ class JsSuspendFunctionsLowering(ctx: JsIrBackendContext) : AbstractSuspendFunct
         val fromType = (delegatingCall as? IrCall)?.symbol?.owner?.returnType ?: delegatingCall.type
         if (!needUnboxingOrUnit(fromType, expectedType)) return delegatingCall
 
-        val ctx = this@JsSuspendFunctionsLowering.context
         return irComposite(resultType = fromType) {
             val tmp = createTmpVariable(delegatingCall, irType = fromType)
-            val coroutineSuspended = irCall(ctx.coroutineSuspendGetter)
+            val coroutineSuspended = irCall(coroutineSymbols.coroutineSuspendedGetter)
             val condition = irEqeqeq(irGet(tmp), coroutineSuspended)
             +irIfThen(fromType, condition, irReturn(irReinterpretCast(irGet(tmp), expectedType)))
             +irGet(tmp)
