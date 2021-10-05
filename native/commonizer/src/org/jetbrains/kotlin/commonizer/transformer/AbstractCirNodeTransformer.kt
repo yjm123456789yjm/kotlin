cdd3c1800fd54c1b26018a8169e73aedcd148767
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2021-10-05 13:11:45 +0300

    [Commonizer] Refactor CIR transformers to make context immutable
    Extract context class for type alias transformers

--- native/commonizer/src/org/jetbrains/kotlin/commonizer/transformer/AbstractCirNodeTransformer.kt
+++ native/commonizer/src/org/jetbrains/kotlin/commonizer/transformer/AbstractCirNodeTransformer.kt
@@ -16,57 +16,56 @@ abstract class AbstractCirNodeTransformer<D : Any> : CirNodeTransformer {
 
     abstract fun newTransformationContext(): D
 
-    private fun processModule(moduleNode: CirModuleNode, moduleName: CirName, context: D) {
-        beforeModule(moduleNode, moduleName, context)
-        moduleNode.packages.values.forEach { processPackage(it, context) }
-        afterModule(moduleNode, moduleName, context)
+    private fun processModule(moduleNode: CirModuleNode, moduleName: CirName, context: D): D {
+        var ctxt = context
+        ctxt = beforeModule(moduleNode, moduleName, ctxt)
+        moduleNode.packages.values.forEach { ctxt = processPackage(it, ctxt) }
+        ctxt = afterModule(moduleNode, moduleName, ctxt)
+
+        return ctxt
     }
 
-    private fun processPackage(packageNode: CirPackageNode, context: D) {
-        beforePackage(packageNode, context)
-        packageNode.properties.values.forEach { processProperty(it, context) }
-        packageNode.functions.values.forEach { processFunction(it, context) }
-        packageNode.classes.values.forEach { processClass(it, context) }
-        packageNode.typeAliases.values.forEach { processTypeAlias(it, context) }
-        afterPackage(packageNode, context)
+    private fun processPackage(packageNode: CirPackageNode, context: D): D {
+        var ctxt = context
+        ctxt = beforePackage(packageNode, ctxt)
+        packageNode.properties.values.forEach { ctxt = processProperty(it, ctxt) }
+        packageNode.functions.values.forEach { ctxt = processFunction(it, ctxt) }
+        packageNode.classes.values.forEach { ctxt = processClass(it, ctxt) }
+        packageNode.typeAliases.values.forEach { ctxt = processTypeAlias(it, ctxt) }
+        afterPackage(packageNode, ctxt)
+
+        return ctxt
     }
 
-    private fun processClass(classNode: CirClassNode, context: D) {
-        beforeClass(classNode, context)
-        classNode.constructors.values.forEach { processConstructor(it, context) }
-        classNode.classes.values.forEach { processClass(it, context) }
-        classNode.functions.values.forEach { processFunction(it, context) }
-        classNode.properties.values.forEach { processProperty(it, context) }
-        afterClass(classNode, context)
+    private fun processClass(classNode: CirClassNode, context: D): D {
+        var ctxt = context
+        ctxt = beforeClass(classNode, ctxt)
+        classNode.constructors.values.forEach { ctxt = processConstructor(it, ctxt) }
+        classNode.classes.values.forEach { ctxt = processClass(it, ctxt) }
+        classNode.functions.values.forEach { ctxt = processFunction(it, ctxt) }
+        classNode.properties.values.forEach { ctxt = processProperty(it, ctxt) }
+        ctxt = afterClass(classNode, ctxt)
+
+        return ctxt
     }
 
-    private fun processProperty(propertyNode: CirPropertyNode, context: D) {
-        transformProperty(propertyNode, context)
-    }
-
-    private fun processFunction(functionNode: CirFunctionNode, context: D) {
-        transformFunction(functionNode, context)
-    }
-
-    private fun processTypeAlias(typeAliasNode: CirTypeAliasNode, context: D) {
-        transformTypeAlias(typeAliasNode, context)
-    }
-
-    private fun processConstructor(constructorNode: CirClassConstructorNode, context: D) {
+    private fun processProperty(propertyNode: CirPropertyNode, context: D): D = transformProperty(propertyNode, context)
+    private fun processFunction(functionNode: CirFunctionNode, context: D): D = transformFunction(functionNode, context)
+    private fun processTypeAlias(typeAliasNode: CirTypeAliasNode, context: D): D = transformTypeAlias(typeAliasNode, context)
+    private fun processConstructor(constructorNode: CirClassConstructorNode, context: D): D =
         transformConstructor(constructorNode, context)
-    }
 
     // TODO: review when the dust settles
-    open fun beforeModule(moduleNode: CirModuleNode, moduleName: CirName, context: D) {}
-    open fun afterModule(moduleNode: CirModuleNode, moduleName: CirName, context: D) {}
-    open fun beforePackage(packageNode: CirPackageNode, context: D) {}
-    open fun afterPackage(packageNode: CirPackageNode, context: D) {}
-    open fun beforeClass(classNode: CirClassNode, context: D) {}
-    open fun afterClass(classNode: CirClassNode, context: D) {}
-    open fun transformProperty(propertyNode: CirPropertyNode, context: D) {}
-    open fun transformFunction(functionNode: CirFunctionNode, context: D) {}
-    open fun transformConstructor(constructorNode: CirClassConstructorNode, context: D) {}
-    open fun transformTypeAlias(typeAliasNode: CirTypeAliasNode, context: D) {}
+    open fun beforeModule(moduleNode: CirModuleNode, moduleName: CirName, context: D): D = context
+    open fun afterModule(moduleNode: CirModuleNode, moduleName: CirName, context: D): D = context
+    open fun beforePackage(packageNode: CirPackageNode, context: D): D = context
+    open fun afterPackage(packageNode: CirPackageNode, context: D): D = context
+    open fun beforeClass(classNode: CirClassNode, context: D): D = context
+    open fun afterClass(classNode: CirClassNode, context: D): D = context
+    open fun transformProperty(propertyNode: CirPropertyNode, context: D): D = context
+    open fun transformFunction(functionNode: CirFunctionNode, context: D): D = context
+    open fun transformConstructor(constructorNode: CirClassConstructorNode, context: D): D = context
+    open fun transformTypeAlias(typeAliasNode: CirTypeAliasNode, context: D): D = context
 }
 
 abstract class AbstractContextlessCirNodeTransformer : AbstractCirNodeTransformer<Unit>() {
