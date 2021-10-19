191db327b763e524d9717671675cf116c209f0bf
Tianyu Geng <tgeng@google.com>
2021-10-19 11:18:39 -0700

    FIR: add pointer from primary constructor param to correspondng property

--- compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
+++ compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
@@ -628,6 +628,7 @@ open class RawFirBuilder(
                 if (firParameter.isVararg) {
                     isFromVararg = true
                 }
+                firParameter.correspondingProperty = this
                 fromPrimaryConstructor = true
             }
         }
