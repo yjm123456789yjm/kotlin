57244a454d7aaa4dee237204a7b268000877a5d3
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-11-01 19:10:13 +0900

    [Pill] Preserve kotlin_test_wasm_js library artifact

--- plugins/pill/pill-importer/src/JpsCompatiblePluginTasks.kt
+++ plugins/pill/pill-importer/src/JpsCompatiblePluginTasks.kt
@@ -70,7 +70,8 @@ class JpsCompatiblePluginTasks(
 
         private val ALLOWED_ARTIFACT_PATTERNS = listOf(
             Regex.fromLiteral("dist_root.xml"),
-            Regex("kotlinx_cli_jvm_[\\d_]+_SNAPSHOT\\.xml")
+            Regex("kotlinx_cli_jvm_[\\d_]+_SNAPSHOT\\.xml"),
+            Regex("kotlin_test_wasm_js_[\\d_]+_SNAPSHOT\\.xml")
         )
     }
 
