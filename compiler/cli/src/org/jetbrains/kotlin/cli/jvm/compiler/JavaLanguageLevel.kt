0281bed353be6e84d8d864a8061bf498ea0b6485
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-11-24 23:14:19 +0900

    Fix exception in the Kotlin IDE plugin built against newer IDEs
    In newer version of IntelliJ IDEA, there's no JDK_15_PREVIEW language
    version anymore. Instead, there's JDK_15. To be able to use the same
    code both in the compiler and in the IDE, the applicable level is
    searched in runtime.

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/JavaLanguageLevel.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/JavaLanguageLevel.kt
@@ -8,7 +8,17 @@ package org.jetbrains.kotlin.cli.jvm.compiler
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.LanguageLevelProjectExtension
 import com.intellij.pom.java.LanguageLevel
+import java.lang.IllegalStateException
 
 fun Project.setupHighestLanguageLevel() {
-    LanguageLevelProjectExtension.getInstance(this).languageLevel = LanguageLevel.JDK_15_PREVIEW
+    val languageLevelExtension = LanguageLevelProjectExtension.getInstance(this)
+
+    val applicableLanguageLevels = listOf("JDK_15_PREVIEW", "JDK_15")
+    for (languageLevelName in applicableLanguageLevels) {
+        val languageLevel = LanguageLevel.values().find { it.name == languageLevelName } ?: continue
+        languageLevelExtension.languageLevel = languageLevel
+        return
+    }
+
+    throw IllegalStateException("Unable to set the latest Java language level, tried $applicableLanguageLevels")
 }
