05215ab01947f76e0fa72ed1269b09a1ebf626c6
Roman Artemev <roman.artemev@jetbrains.com>
2021-11-25 13:04:09 +0300

    fix typo

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/ToplevelPhases.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/ToplevelPhases.kt
@@ -219,7 +219,7 @@ internal val serializerPhase = konanUnitPhase(
         op = {
             val expectActualLinker = config.configuration.get(CommonConfigurationKeys.EXPECT_ACTUAL_LINKER) ?: false
             val messageLogger = config.configuration.get(IrMessageLogger.IR_MESSAGE_LOGGER) ?: IrMessageLogger.None
-            val relativePathBase = config.configuration.get(CommonConfigurationKeys.RELATIVE_PATH_BASES)?.let { it[0]
+            val relativePathBase = config.configuration.get(CommonConfigurationKeys.RELATIVE_PATH_BASES)?.let { it[0] }
 
                 serializedIr = irModule?.let { ir ->
                 KonanIrModuleSerializer(
