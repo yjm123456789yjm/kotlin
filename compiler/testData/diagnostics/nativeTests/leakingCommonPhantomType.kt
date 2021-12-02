c84c0fa3fdea3033a9779a2935fd03e50847c926
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2021-12-02 16:10:44 +0300

    Fix diagnostic test for phantom type checker after renaming

--- compiler/testData/diagnostics/nativeTests/leakingCommonPhantomType.kt
+++ compiler/testData/diagnostics/nativeTests/leakingCommonPhantomType.kt
@@ -6,96 +6,96 @@
 
 package kotlin
 
-public interface SignedInteger
+public interface PlatformInt
 
 // FILE: phantom_type_producer.kt
 
-import kotlin.<!PHANTOM_CLASSIFIER!>SignedInteger<!>
+import kotlin.<!PHANTOM_CLASSIFIER!>PlatformInt<!>
 
-fun <<!LEAKING_PHANTOM_TYPE_IN_TYPE_PARAMETERS!>T : <!PHANTOM_CLASSIFIER!>SignedInteger<!><!>> topLevelFn1(
-    arg: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>SignedInteger<!>
-): <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>SignedInteger<!> {
+fun <<!LEAKING_PHANTOM_TYPE_IN_TYPE_PARAMETERS!>T : <!PHANTOM_CLASSIFIER!>PlatformInt<!><!>> topLevelFn1(
+    arg: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>PlatformInt<!>
+): <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>PlatformInt<!> {
     null!!
 }
 
 fun <!LEAKING_PHANTOM_TYPE!>topLevelFn2<!>() =
-    null as <!PHANTOM_CLASSIFIER!>SignedInteger<!>
+    null as <!PHANTOM_CLASSIFIER!>PlatformInt<!>
 
-fun <<!LEAKING_PHANTOM_TYPE_IN_TYPE_PARAMETERS!>T<!>> topLevelFn3(): Any where T : Number, T : <!PHANTOM_CLASSIFIER!>SignedInteger<!> {
+fun <<!LEAKING_PHANTOM_TYPE_IN_TYPE_PARAMETERS!>T<!>> topLevelFn3(): Any where T : Number, T : <!PHANTOM_CLASSIFIER!>PlatformInt<!> {
     null!!
 }
 
-val topLevelProp1: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>SignedInteger<!> =
+val topLevelProp1: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>PlatformInt<!> =
     null!!
 
 val <!LEAKING_PHANTOM_TYPE!>topLevelProp2<!> =
-    null as <!PHANTOM_CLASSIFIER!>SignedInteger<!>
+    null as <!PHANTOM_CLASSIFIER!>PlatformInt<!>
 
 interface Generic<T>
 interface Stub
 
-class Class<<!LEAKING_PHANTOM_TYPE_IN_TYPE_PARAMETERS!>T : <!PHANTOM_CLASSIFIER!>SignedInteger<!><!>>(
-    val prop1: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>SignedInteger<!>,
-    val prop2: <!LEAKING_PHANTOM_TYPE!>List<Set<Collection<<!PHANTOM_CLASSIFIER!>SignedInteger<!>>>><!>,
-    <!VALUE_PARAMETER_WITH_NO_TYPE_ANNOTATION!>val propNoTypeReference<!SYNTAX!><!> = null as <!PHANTOM_CLASSIFIER!>SignedInteger<!><!>,
-) : <!LEAKING_PHANTOM_TYPE_IN_SUPERTYPES!>Stub, Generic<<!PHANTOM_CLASSIFIER!>SignedInteger<!>><!> {
-    class NestedClass<<!LEAKING_PHANTOM_TYPE_IN_TYPE_PARAMETERS!>T: <!PHANTOM_CLASSIFIER!>SignedInteger<!><!>>(
-        val ncProp: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>SignedInteger<!>
+class Class<<!LEAKING_PHANTOM_TYPE_IN_TYPE_PARAMETERS!>T : <!PHANTOM_CLASSIFIER!>PlatformInt<!><!>>(
+    val prop1: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>PlatformInt<!>,
+    val prop2: <!LEAKING_PHANTOM_TYPE!>List<Set<Collection<<!PHANTOM_CLASSIFIER!>PlatformInt<!>>>><!>,
+    <!VALUE_PARAMETER_WITH_NO_TYPE_ANNOTATION!>val propNoTypeReference<!SYNTAX!><!> = null as <!PHANTOM_CLASSIFIER!>PlatformInt<!><!>,
+) : <!LEAKING_PHANTOM_TYPE_IN_SUPERTYPES!>Stub, Generic<<!PHANTOM_CLASSIFIER!>PlatformInt<!>><!> {
+    class NestedClass<<!LEAKING_PHANTOM_TYPE_IN_TYPE_PARAMETERS!>T: <!PHANTOM_CLASSIFIER!>PlatformInt<!><!>>(
+        val ncProp: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>PlatformInt<!>
     ) {
-        fun <!LEAKING_PHANTOM_TYPE!>ncFun<!>() = null as <!PHANTOM_CLASSIFIER!>SignedInteger<!>
+        fun <!LEAKING_PHANTOM_TYPE!>ncFun<!>() = null as <!PHANTOM_CLASSIFIER!>PlatformInt<!>
     }
 
-    inner class InnerClass<<!LEAKING_PHANTOM_TYPE_IN_TYPE_PARAMETERS!>T: <!PHANTOM_CLASSIFIER!>SignedInteger<!><!>>(
-        val icProp: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>SignedInteger<!>
+    inner class InnerClass<<!LEAKING_PHANTOM_TYPE_IN_TYPE_PARAMETERS!>T: <!PHANTOM_CLASSIFIER!>PlatformInt<!><!>>(
+        val icProp: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>PlatformInt<!>
     ) {
-        fun <!LEAKING_PHANTOM_TYPE!>icFun<!>() = null as <!PHANTOM_CLASSIFIER!>SignedInteger<!>
+        fun <!LEAKING_PHANTOM_TYPE!>icFun<!>() = null as <!PHANTOM_CLASSIFIER!>PlatformInt<!>
     }
 
     fun <!LEAKING_PHANTOM_TYPE!>escapeFromLocalClass<!>() = {
-        class Local<T : <!PHANTOM_CLASSIFIER!>SignedInteger<!>>(
+        class Local<T : <!PHANTOM_CLASSIFIER!>PlatformInt<!>>(
             private val prop: T,
-            val lcProp: <!PHANTOM_CLASSIFIER!>SignedInteger<!>,
+            val lcProp: <!PHANTOM_CLASSIFIER!>PlatformInt<!>,
         ) {
-            fun lcFun() = null as <!PHANTOM_CLASSIFIER!>SignedInteger<!>
+            fun lcFun() = null as <!PHANTOM_CLASSIFIER!>PlatformInt<!>
             fun escape(): T = prop
         }
 
-        Local(null as <!PHANTOM_CLASSIFIER!>SignedInteger<!>, null as <!PHANTOM_CLASSIFIER!>SignedInteger<!>).escape()
+        Local(null as <!PHANTOM_CLASSIFIER!>PlatformInt<!>, null as <!PHANTOM_CLASSIFIER!>PlatformInt<!>).escape()
     }()
 
     val <!LEAKING_PHANTOM_TYPE!>prop3<!> =
-        null as <!PHANTOM_CLASSIFIER!>SignedInteger<!>
-    val prop4: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>SignedInteger<!>
+        null as <!PHANTOM_CLASSIFIER!>PlatformInt<!>
+    val prop4: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>PlatformInt<!>
         get() = null!!
-    var prop5: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>SignedInteger<!> =
+    var prop5: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>PlatformInt<!> =
         null!!
-    var prop6: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>SignedInteger<!>
+    var prop6: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>PlatformInt<!>
         get() = null!!
         set(value) { null!! }
 
-    fun <<!LEAKING_PHANTOM_TYPE_IN_TYPE_PARAMETERS!>T: <!PHANTOM_CLASSIFIER!>SignedInteger<!><!>> member1(
-        arg1: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>SignedInteger<!>,
+    fun <<!LEAKING_PHANTOM_TYPE_IN_TYPE_PARAMETERS!>T: <!PHANTOM_CLASSIFIER!>PlatformInt<!><!>> member1(
+        arg1: <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>PlatformInt<!>,
         arg2: T
-    ): <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>SignedInteger<!> {
+    ): <!LEAKING_PHANTOM_TYPE, PHANTOM_CLASSIFIER!>PlatformInt<!> {
         null!!
     }
 
     fun <!LEAKING_PHANTOM_TYPE!>implicitLambdaType<!>() = {
-        null as Map<Any, List<<!PHANTOM_CLASSIFIER!>SignedInteger<!>>>
+        null as Map<Any, List<<!PHANTOM_CLASSIFIER!>PlatformInt<!>>>
     }
 
 
     /////////////// Shouldn't be reported for local usages/instances ///////////////
 
     fun hidden1(): Any {
-        val localProp = null as <!PHANTOM_CLASSIFIER!>SignedInteger<!>
+        val localProp = null as <!PHANTOM_CLASSIFIER!>PlatformInt<!>
         val fromPrivateFun = hidden3()
         return localProp
     }
 
     fun hidden2(): Any =
-        null as <!PHANTOM_CLASSIFIER!>SignedInteger<!>
+        null as <!PHANTOM_CLASSIFIER!>PlatformInt<!>
 
-    private fun hidden3() = null as <!PHANTOM_CLASSIFIER!>SignedInteger<!>
+    private fun hidden3() = null as <!PHANTOM_CLASSIFIER!>PlatformInt<!>
 
 }
