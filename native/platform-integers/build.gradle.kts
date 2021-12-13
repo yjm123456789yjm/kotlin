aa6b77c6ad68895baf1ca97314932fc942d3d76e
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2021-12-13 17:11:56 +0300

    [Commonizer] Add remaining native targets for platform integers

--- native/platform-integers/build.gradle.kts
+++ native/platform-integers/build.gradle.kts
@@ -12,12 +12,23 @@ kotlin {
     linuxArm32Hfp()
     linuxMips32()
     linuxMipsel32()
-    macosX64()
+    linuxArm64()
     iosArm32()
     iosArm64()
+    iosSimulatorArm64()
     iosX64()
     mingwX86()
     mingwX64()
+    macosX64()
+    macosArm64()
+    watchosArm32()
+    watchosArm64()
+    watchosX86()
+    watchosX64()
+    watchosSimulatorArm64()
+    tvosArm64()
+    tvosX64()
+    tvosSimulatorArm64()
 
     sourceSets {
         val commonMain by getting
@@ -33,12 +44,23 @@ kotlin {
         val linuxArm32HfpMain by getting
         val linuxMips32Main by getting
         val linuxMipsel32Main by getting
+        val linuxArm64Main by getting
         val macosX64Main by getting
         val iosArm32Main by getting
         val iosArm64Main by getting
         val iosX64Main by getting
         val mingwX64Main by getting
         val mingwX86Main by getting
+        val iosSimulatorArm64Main by getting
+        val macosArm64Main by getting
+        val watchosArm32Main by getting
+        val watchosArm64Main by getting
+        val watchosX86Main by getting
+        val watchosX64Main by getting
+        val watchosSimulatorArm64Main by getting
+        val tvosArm64Main by getting
+        val tvosX64Main by getting
+        val tvosSimulatorArm64Main by getting
 
         val bitness32 = listOf(
             linuxArm32HfpMain,
@@ -47,6 +69,8 @@ kotlin {
             linuxMipsel32Main,
             iosArm32Main,
             mingwX86Main,
+            watchosArm32Main,
+            watchosX86Main,
         )
         val bitness64 = listOf(
             linuxX64Main,
@@ -54,6 +78,15 @@ kotlin {
             iosArm64Main,
             iosX64Main,
             mingwX64Main,
+            linuxArm64Main,
+            iosSimulatorArm64Main,
+            macosArm64Main,
+            watchosArm64Main,
+            watchosX64Main,
+            watchosSimulatorArm64Main,
+            tvosArm64Main,
+            tvosX64Main,
+            tvosSimulatorArm64Main,
         )
 
         bitness32.forEach { sourceSet ->
