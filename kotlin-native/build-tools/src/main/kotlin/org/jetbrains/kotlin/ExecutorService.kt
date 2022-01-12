934b9a04e08acd62617bd2495516d516ba866973
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2022-01-12 11:06:48 +0300

    [K/N] Support compile-only mode for legacy test infra

--- kotlin-native/build-tools/src/main/kotlin/org/jetbrains/kotlin/ExecutorService.kt
+++ kotlin-native/build-tools/src/main/kotlin/org/jetbrains/kotlin/ExecutorService.kt
@@ -51,11 +51,25 @@ fun create(project: Project): ExecutorService {
 
     return when {
         project.hasProperty("remote") -> sshExecutor(project, testTarget)
+        project.compileOnlyTests -> noopExecutor(project)
         configurables is WasmConfigurables -> wasmExecutor(project)
         configurables is ConfigurablesWithEmulator && testTarget != HostManager.host -> emulatorExecutor(project, testTarget)
         configurables.targetTriple.isSimulator -> simulator(project)
         supportsRunningTestsOnDevice(testTarget) -> deviceLauncher(project)
         else -> localExecutorService(project)
+    }
+}
+
+private fun noopExecutor(project: Project) = object : ExecutorService {
+    override val project: Project
+        get() = project
+
+    override fun execute(action: Action<in ExecSpec>): ExecResult? = object : ExecResult {
+        override fun getExitValue(): Int = 0
+
+        override fun assertNormalExitValue(): ExecResult = this
+
+        override fun rethrowFailure(): ExecResult = this
     }
 }
 
