64c405ced7a9126590e8cf8d124df7cfcea6edc8
Nikita Bobko <nikita.bobko@jetbrains.com>
2022-05-26 21:02:21 +0200

    Introduce `kotlin-jps-plugin` and deprecate `kotlin-jps-plugin-classpath`
    `kotlin-jps-plugin-classpath` isn't dropped because, in some places, IDE
    depends on the old artifact so I want to keep it for a while until I
    cleanup IDE completely.
    
    I tried to put as much libraries into `kotlinJpsPluginMavenDependencies`
    as possible in the previous commit. Right now,
    `kotlin-jps-plugin-classpath` is 33Mb, `kotlin-jps-plugin` is 20Mb (all
    are not compresed)
    
    (cherry picked from commit 18feb3056937f0621c28bb8c3cf2b13022e3802b)

--- build.gradle.kts
+++ build.gradle.kts
@@ -327,6 +327,7 @@ extra["compilerArtifactsForIde"] = listOf(
     ":prepare:ide-plugin-dependencies:kotlin-compiler-common-for-ide",
     ":prepare:ide-plugin-dependencies:kotlin-compiler-fe10-for-ide",
     ":prepare:ide-plugin-dependencies:kotlin-compiler-fir-for-ide",
+    ":prepare:kotlin-jps-plugin",
     ":kotlin-script-runtime",
     ":kotlin-script-util",
     ":kotlin-scripting-common",
