71f6898ee82df50abf40aae16244997d373cd458
Nikita Bobko <nikita.bobko@jetbrains.com>
2022-05-27 07:27:53 +0200

    Refactoring: "suck in" `kotlin-reflect` and `kotlin-daemon-client` into `compilerModulesForJps`
    This commit places all dependencies of JPS plugin into a single place --
    `compilerModulesForJps`. I will need this small refactoring for the next
    commits.
    
    This commit doesn't change any logic because
    `prepare/ide-plugin-dependencies/kotlin-jps-plugin-classpath/build.gradle.kts`
    is the only `compilerModulesForJps` user right now.
    
    (cherry picked from commit 6bbd0075600832a93b9b6068e150c97dec773ae4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -256,6 +256,8 @@ extra["compilerModules"] =
 
 
 extra["compilerModulesForJps"] = listOf(
+    ":kotlin-daemon-client",
+    ":kotlin-reflect",
     ":kotlin-build-common",
     ":kotlin-util-io",
     ":kotlin-util-klib",
