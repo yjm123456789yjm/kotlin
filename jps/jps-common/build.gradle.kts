3d98936aa78945a15d762d10993e51f54a70dc7b
Nikita Bobko <nikita.bobko@jetbrains.com>
2022-06-01 17:18:59 +0200

    Cherry-pick jps/ from ij-1.6.0 branch
    This commit is generated by this command:
        git checkout origin/ij-1.6.0 -- \
            build.gradle.kts \
            buildSrc \
            compiler/incremental-compilation-impl/test \
            compiler/tests-compiler-utils/tests \
            gradle/verification-metadata.xml \
            jps \
            prepare/ide-plugin-dependencies \
            settings.gradle \
            tests/mute-common.csv \
    
    origin/ij-1.6.0 is de40d2a9df5
    
    List of not applied changes (in case you want to review them):
        git diff --numstat v1.6.10..origin/ij-1.6.0 -- \
            ':!build.gradle.kts' \
            ':!buildSrc' \
            ':!compiler/incremental-compilation-impl/test' \
            ':!compiler/tests-compiler-utils/tests' \
            ':!gradle/verification-metadata.xml' \
            ':!jps' \
            ':!prepare/ide-plugin-dependencies' \
            ':!settings.gradle' \
            ':!tests/mute-common.csv' \
    
    Note: 22 tests fail in ./gradlew :jps:jps-plugin:test
        org.jetbrains.kotlin.jps.build.AsmVersionForJpsBuildTest > testAsmVersionForBundledKotlinCompiler FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJsJpsTestGenerated > testClassAdded FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJsJpsTestGenerated > testClassRemoved FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJsJpsTestGenerated > testCopyFileToAnotherModule FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJsJpsTestGenerated > testDuplicatedClass FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJsJpsTestGenerated > testFunctionFromDifferentPackageChanged FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJsJpsTestGenerated > testInlineFunctionInlined FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJsJpsTestGenerated > testInlineFunctionTwoPackageParts FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJsJpsTestGenerated > testMoveFileToAnotherModule FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJsJpsTestGenerated > testSimpleDependency FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJsJpsTestGenerated > testSimpleDependencyErrorOnAccessToInternal1 FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJsJpsTestGenerated > testSimpleDependencyErrorOnAccessToInternal2 FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJsJpsTestGenerated > testTransitiveInlining FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJvmJpsTestGenerated$WithJava$JavaUsedInKotlin > testRawErrorTypeDuringSerialization FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJvmJpsTestGenerated$WithJava$JavaUsedInKotlin$SamConversions > testMethodAddDefault FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJvmJpsTestGenerated$WithJava$Other > testMultifileClassAddTopLevelFunWithDefault FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJvmJpsTestGenerated$WithJava$Other > testMultifileClassFileChanged FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJvmJpsTestGenerated$WithJava$Other > testMultifilePackagePartMethodAdded FAILED
        org.jetbrains.kotlin.jps.build.IncrementalJvmJpsTestGenerated$WithJava$Other > testMultifilePartsWithProperties FAILED
        org.jetbrains.kotlin.jps.build.KotlinJpsBuildTest > testSourcePackageLongPrefix FAILED
        org.jetbrains.kotlin.jps.build.KotlinJpsBuildTestIncremental > testSourcePackageLongPrefix FAILED
        org.jetbrains.kotlin.jps.build.RelocatableJpsCachesTest$RelocatableCacheTestCaseIml > junit.framework.TestSuite$1.warning FAILED
    
    ^ Same tests fail in origin/ij-1.6.0
    
    (cherry picked from commit 1579b6a56d900ceb14293e800793f0bc31bbfece)

---
+++ jps/jps-common/build.gradle.kts
@@ -0,0 +1,26 @@
+
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+dependencies {
+    compile(kotlinStdlib())
+    compileOnly(project(":kotlin-reflect-api"))
+    compile(project(":compiler:util"))
+    compile(project(":compiler:cli-common"))
+    compile(project(":compiler:frontend.java"))
+    compile(project(":js:js.frontend"))
+    compile(project(":native:frontend.native"))
+    compileOnly(intellijDep())
+    compileOnly(jpsStandalone()) { includeJars("jps-model") }
+
+    testCompile(commonDep("junit:junit"))
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" { projectDefault() }
+}
+
+runtimeJar()
