d1de131fe761f9d1c1f2b9d6ede49168d132ab32
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2022-06-14 17:10:33 +0200

    Cache native compiler classloader.

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/tasks/NativeCompilerWorkAction.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/tasks/NativeCompilerWorkAction.kt
@@ -7,17 +7,22 @@ package org.jetbrains.kotlin.gradle.targets.native.tasks
 
 import org.gradle.api.Project
 import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.FileCollection
 import org.gradle.api.provider.ListProperty
 import org.gradle.workers.WorkAction
 import org.gradle.workers.WorkParameters
 import org.gradle.workers.WorkerExecutor
 import org.jetbrains.kotlin.compilerRunner.KotlinNativeCompilerRunner
 import java.net.URLClassLoader
+import java.util.*
+import java.util.concurrent.locks.ReentrantReadWriteLock
+import kotlin.concurrent.read
+import kotlin.concurrent.write
 
 internal abstract class NativeCompilerWorkAction : WorkAction<NativeCompilerWorkAction.NativeCompilerArguments> {
     override fun execute() {
         // We need isolated classloader here to avoid using Kotlin runtime coming via Gradle dependencies
-        val classloader = URLClassLoader(parameters.compilerClasspath.files.map { it.toURI().toURL() }.toTypedArray(), null)
+        val classloader = getCompilerClassloader(parameters.compilerClasspath)
         val nativeMain = Class.forName("org.jetbrains.kotlin.cli.utilities.MainKt", true, classloader)
         val mainMethod = nativeMain.declaredMethods.single { it.name == "main" }
         mainMethod.invoke(nativeMain, parameters.buildArgs.get().toTypedArray())
@@ -29,6 +34,25 @@ internal abstract class NativeCompilerWorkAction : WorkAction<NativeCompilerWork
     }
 
     companion object {
+        private lateinit var cachedCompilerClassloader: ClassLoader
+        private var compilerClasspathHash: Int = 0
+        private val classpathLock = ReentrantReadWriteLock()
+
+        fun getCompilerClassloader(classpath: FileCollection): ClassLoader {
+            classpathLock.read {
+                val classpathHash = Objects.hash(*classpath.asFileTree.files.map { it.absolutePath }.toTypedArray())
+                return if (classpathHash == compilerClasspathHash) {
+                    cachedCompilerClassloader
+                } else {
+                    classpathLock.write {
+                        cachedCompilerClassloader = URLClassLoader(classpath.files.map { it.toURI().toURL() }.toTypedArray(), null)
+                        compilerClasspathHash = classpathHash
+                    }
+                    cachedCompilerClassloader
+                }
+            }
+        }
+
         fun runViaWorker(
             project: Project,
             workerExecutor: WorkerExecutor,
@@ -36,7 +60,6 @@ internal abstract class NativeCompilerWorkAction : WorkAction<NativeCompilerWork
         ) {
             val nativeCompilerRunner = KotlinNativeCompilerRunner(project)
             val workQueue = workerExecutor.processIsolation { spec ->
-                spec.classpath.setFrom(nativeCompilerRunner.classpath)
                 spec.forkOptions {
                     it.jvmArgs = nativeCompilerRunner.jvmArgs
                     it.enableAssertions = nativeCompilerRunner.enableAssertions
