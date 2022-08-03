b4befe7a4abe783c44139bd3cacba381d20c6fc4
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-08-03 16:00:15 +0300

    [AA LC] Implement default values for methods of annotations

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/symbols/firSymbolUtils.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/symbols/firSymbolUtils.kt
@@ -16,11 +16,10 @@ import org.jetbrains.kotlin.analysis.api.impl.base.KtContextReceiverImpl
 import org.jetbrains.kotlin.analysis.api.symbols.KtValueParameterSymbol
 import org.jetbrains.kotlin.analysis.api.types.KtType
 import org.jetbrains.kotlin.analysis.utils.printer.getElementTextInContext
-import org.jetbrains.kotlin.fir.declarations.FirContextReceiver
-import org.jetbrains.kotlin.fir.declarations.FirDeclaration
-import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
-import org.jetbrains.kotlin.fir.declarations.FirTypeParameterRefsOwner
+import org.jetbrains.kotlin.fir.declarations.*
+import org.jetbrains.kotlin.fir.expressions.FirPropertyAccessExpression
 import org.jetbrains.kotlin.fir.psi
+import org.jetbrains.kotlin.fir.references.impl.FirPropertyFromParameterResolvedNamedReference
 import org.jetbrains.kotlin.fir.renderer.FirRenderer
 import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
 import org.jetbrains.kotlin.fir.symbols.lazyResolveToPhase
@@ -89,8 +88,18 @@ internal fun FirCallableSymbol<*>.dispatchReceiverType(
     return dispatchReceiverType?.let { builder.typeBuilder.buildKtType(it) }
 }
 
-internal fun FirVariableSymbol<*>.getKtConstantInitializer(): KtInitializerValue? {
+internal fun FirVariableSymbol<*>.getKtConstantInitializer(forAnnotationDefaultValue: Boolean): KtInitializerValue? {
     lazyResolveToPhase(FirResolvePhase.BODY_RESOLVE)
-    val firInitializer = fir.initializer ?: return null
-    return firInitializer.asKtInitializerValue()
+    var firInitializer = fir.initializer ?: return null
+    if (firInitializer is FirPropertyAccessExpression) {
+        val calleeReference = firInitializer.calleeReference
+        if (calleeReference is FirPropertyFromParameterResolvedNamedReference) {
+            val valueParameterSymbol = calleeReference.resolvedSymbol as? FirValueParameterSymbol
+            if (valueParameterSymbol != null) {
+                valueParameterSymbol.lazyResolveToPhase(FirResolvePhase.BODY_RESOLVE)
+                firInitializer = valueParameterSymbol.fir.defaultValue ?: firInitializer
+            }
+        }
+    }
+    return firInitializer.asKtInitializerValue(moduleData.session, forAnnotationDefaultValue)
 }
