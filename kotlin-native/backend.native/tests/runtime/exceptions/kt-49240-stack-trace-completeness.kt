5763d20afa8bc1210c83ce315f54c7bf44277588
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2022-08-18 15:54:30 +0200

    Add test

---
+++ kotlin-native/backend.native/tests/runtime/exceptions/kt-49240-stack-trace-completeness.kt
@@ -0,0 +1,43 @@
+import kotlin.text.Regex
+import kotlin.test.*
+
+var inlinesCount = 0
+
+fun exception() {
+    error("FAIL!")
+}
+
+fun main() {
+    try {
+        exception()
+    }
+    catch (e:Exception) {
+        val stackTrace = e.getStackTrace().filter { "kfun:" in it }
+        println("Kotlin part of call stack is:")
+        for (entry in stackTrace)
+            println(entry)
+        println("Verifying...")
+        val goldValues = arrayOf(
+                "kfun:kotlin.Throwable#<init>(kotlin.String?){}",
+                "kfun:kotlin.Exception#<init>(kotlin.String?){}",
+                "kfun:kotlin.RuntimeException#<init>(kotlin.String?){}",
+                "kfun:kotlin.IllegalStateException#<init>(kotlin.String?){}",
+                "kfun:#exception(){}",
+                "kfun:#main(){}",
+        )
+        assertEquals( goldValues.size, stackTrace.size )
+        goldValues.zip(stackTrace).forEach { checkFrame(it) }
+        println("Passed")
+    }
+}
+
+internal val regex = Regex("(kfun.+) \\+ (\\d+)")
+internal fun checkFrame(pair: Pair<String, String>) {
+    val goldFunName = pair.first
+    val actualLine = pair.second
+    val findResult = regex.find(actualLine)
+
+    val (funName, offset) = findResult?.destructured ?: throw Error("Cannot find '$goldFunName + <int>' in $actualLine")
+    assertEquals( goldFunName, funName )
+    assert( offset.toInt() > 0 )
+}
