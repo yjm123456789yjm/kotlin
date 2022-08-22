d80f9260d4a26f5e6d61ccd82795963312f15fe0
Sebastian Sellmair <sebastian.sellmair@gmail.com>
2022-08-22 13:27:23 +0200

    [Gradle][MPP] Add documentation to `withNaturalHierarchy`
    ^KT-53570 Verification Pending

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/hierarchy/withNaturalHierarchy.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/hierarchy/withNaturalHierarchy.kt
@@ -9,6 +9,40 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinTargetContainerWithPresetFunctions
 import org.jetbrains.kotlin.gradle.plugin.ExperimentalKotlinGradlePluginApi
 
+/**
+ * Targets declared within this scope will automatically create shared Kotlin source sets based upon
+ * the 'natural' hierarchy:
+ *
+ * ```
+ *                                                   common
+ *
+ *                                                     |
+ *         +----------------------+--------------------+-----------------------+-------------------------+
+ *         |                      |                    |                       |                         |
+ *                                                                                                       |
+ *       apple                  linux              windows              androidNative                   ...
+ *
+ *         |
+ *  +------+----+------------+------------+
+ *  |           |            |            |
+ *
+ * macos       ios         tvos        watchos
+ * ```
+ *
+ * ### Example: Sharing code for ios
+ *
+ * ```kotlin
+ * kotlin {
+ *        withNaturalHierarchy {
+ *            iosX64()
+ *            iosArm64()
+ *            iosSimulatorArm64()
+ *        }
+ * }
+ * ```
+ *
+ * This will automatically create all shared source sets in the tree above (e.g. `iosMain` and `iosTest`)
+ */
 @ExperimentalKotlinGradlePluginApi
 fun KotlinMultiplatformExtension.withNaturalHierarchy(
     configure: KotlinTargetContainerWithHierarchy.() -> Unit
