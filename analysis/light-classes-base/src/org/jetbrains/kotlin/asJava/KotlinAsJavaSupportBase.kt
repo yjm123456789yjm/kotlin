d00fd2f858b72ce8018de2b5de69ea34618dc87a
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-08-22 14:41:43 +0200

    [lc] support decompiled facade only for .class
    ^KT-53543

--- analysis/light-classes-base/src/org/jetbrains/kotlin/asJava/KotlinAsJavaSupportBase.kt
+++ analysis/light-classes-base/src/org/jetbrains/kotlin/asJava/KotlinAsJavaSupportBase.kt
@@ -16,12 +16,11 @@ import org.jetbrains.kotlin.fileClasses.isJvmMultifileClassFile
 import org.jetbrains.kotlin.fileClasses.javaFileFacadeFqName
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtFile
-import org.jetbrains.kotlin.serialization.deserialization.builtins.BuiltInSerializerProtocol
 
 abstract class KotlinAsJavaSupportBase<TModule>(protected val project: Project) : KotlinAsJavaSupport() {
     fun createLightFacade(file: KtFile): KtLightClassForFacade? {
         if (file.isScript()) return null
-        if (file.isCompiled && file.name.endsWith(BuiltInSerializerProtocol.BUILTINS_FILE_EXTENSION)) return null
+        if (file.isCompiled && !file.name.endsWith(".class")) return null
 
         val module = file.findModule().takeIf { it.isApplicable() } ?: return null
         val facadeFqName = file.javaFileFacadeFqName
